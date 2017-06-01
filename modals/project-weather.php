ff26a0e7c353e664841adce88f98624a3490869c
Tiffany Du <tadofdesign@gmail.com>
2017-06-01 18:03:02 -0500

    Created Weather project modal

---
+++ modals/project-weather.php
@@ -0,0 +1,61 @@
+  <!-- Weather App Project Modal -->
+  <div id="weatherModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="weatherTitle">
+    <div class="modal-dialog wide" role="document">
+      <div class="modal-content text-left">
+        <div class="modal-header text-center">
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <!-- <h3 id="weatherTitle" class="modal-title text-center">Project Details</h3> -->
+          <h2 id="weatherTitle" class="modal-title">Local Weather App</h2>
+          <h4>Web Development | UI Design</h4>
+        </div><!-- /.modal-header -->
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-sm-12">
+              <img class="img-responsive center-block" src="assets/portfolio/weather/weather-hero-lg.jpg" alt="Weather app hero">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-4">
+              <img class="img-responsive center-block" src="assets/portfolio/weather/weather-sketch1.jpg" alt="Weather app sketch">
+            </div>
+            <div class="col-sm-4">
+              <img class="img-responsive center-block" src="assets/portfolio/weather/weather-sketch2.jpg" alt="Weather app sketch">
+            </div>
+            <div class="col-sm-4">
+              <img class="img-responsive center-block" src="assets/portfolio/weather/weather-sketch3.jpg" alt="Weather app sketch">
+            </div>
+          </div> <!-- /.row -->
+          <div class="row">
+            <div class="col-sm-12 text-center">
+              <!-- <h2>Local Weather App</h2>
+              <h4>Web Development | UI Design</h4> -->
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-10 col-sm-offset-1 col-lg-6 col-lg-offset-0">
+              <p>Some descriptive text here.</p>
+              <p class="mb-30"></p>
+            </div>
+            <div class="col-sm-10 col-sm-offset-1 col-lg-6 col-lg-offset-0 md-center">
+              <p><strong>Technologies Used:</strong></p>
+              <p class="mb-30">
+                <span class="label tag">Responsive Design</span>
+                <span class="label tag">HTML5</span>
+                <span class="label tag">CSS3</span>
+                <span class="label tag">JavaScript</span>
+                <span class="label tag">jQuery</span>
+                <span class="label tag">AJAX</span>
+                <span class="label tag">Geolocation</span>
+                <span class="label tag">Wunderground API</span>
+              </p>
+              <a class="btn td-btn outline green mb-30" href="https://tiffanyadu.github.io/weather-app/" target="_blank" title="Link to Local Weather App"><span class="glyphicon glyphicon-link"></span> Visit Live Site</a>
+              <a class="btn td-btn outline green mb-30 iconBtn" href="https://github.com/tiffanyadu/weather-app" target="_blank" title="Link to Local Weather App on GitHub"><img src="assets/icons/social/github-green.svg" alt="GitHub Icon">View on GitHub</a>
+            </div>
+          </div><!-- /.row -->
+          <div class="modal-footer center">
+            <button class="btn td-btn outline small" type="button" data-dismiss="modal">Close Project</button>
+          </div><!-- /.modal-footer -->
+        </div><!-- /.modal-body -->
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
