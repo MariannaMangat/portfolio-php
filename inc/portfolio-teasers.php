e40002414fd01addb7cf17f81a5b1cc3b569fd10
Tiffany Du <tadofdesign@gmail.com>
2017-06-01 18:03:53 -0500

    Minor updates to portfolio teasers

--- inc/portfolio-teasers.php
+++ inc/portfolio-teasers.php
@@ -3,10 +3,27 @@
     <div class="col-xs-12">
       <a class="teaser" href="#" data-toggle="modal" data-target="#tributeModal">
         <div>
-          <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/weather.png" alt="Weather app teaser">
+          <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/portfolio-teaser.jpg" alt="Personal Portfolio teaser">
           <div class="overlay">
             <div>
-              <h3>Weather App</h3>
+              <h3>Personal Portfolio</h3>
+              <h4>Web Development | UI/UX Design | Logo &amp; Branding</h4>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+<div class="col-sm-6 col-lg-4">
+  <div class="row">
+    <div class="col-xs-12">
+      <a class="teaser" href="#" data-toggle="modal" data-target="#weatherModal">
+        <div>
+          <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/weather-teaser.jpg" alt="Weather app teaser">
+          <div class="overlay">
+            <div>
+              <h3>Local Weather App</h3>
               <h4>Web Development | UI Design</h4>
             </div>
           </div>
@@ -20,11 +37,11 @@
     <div class="col-xs-12">
       <a class="teaser" href="#" data-toggle="modal" data-target="#tributeModal">
         <div>
-          <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/weather2.png" alt="teaser">
+          <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/quote-teaser.jpg" alt="Random design quote teaser">
           <div class="overlay">
             <div>
-              <h3>Twenty One Pilots Tribute</h3>
-              <h4>Responsive Web Design</h4>
+              <h3>Random Design Quotes</h3>
+              <h4>Web Development</h4>
             </div>
           </div>
         </div>
@@ -37,28 +54,11 @@
     <div class="col-xs-12">
       <a class="teaser" href="#" data-toggle="modal" data-target="#tributeModal">
         <div>
-          <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/weather3.png" alt="teaser">
+          <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/tribute-teaser.jpg" alt="Twenty One Pilots tribute teaser">
           <div class="overlay">
               <div>
-              <h3>Project Title</h3>
-              <h4>Responsive Web Design</h4>
-            </div>
-          </div>
-        </div>
-      </a>
-    </div>
-  </div>
-</div>
-<div class="col-sm-6 col-lg-4">
-  <div class="row">
-    <div class="col-xs-12">
-      <a class="teaser" href="#" data-toggle="modal" data-target="#tributeModal">
-        <div>
-          <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/weather4.png" alt="teaser">
-          <div class="overlay">
-            <div>
-              <h3>Project Title</h3>
-              <h4>Responsive Web Design</h4>
+              <h3>Twenty One Pilots Tribute</h3>
+              <h4>Web Development</h4>
             </div>
           </div>
         </div>
