1c8c9a1273ecfb11b4ccfab2ec1cc901c942a643
Tiffany Du <tadofdesign@gmail.com>
2017-06-09 15:35:48 -0500

    Updated config file for live site

--- inc/config.php
+++ inc/config.php
@@ -1,8 +1,8 @@
 <?php
 // Use for local development
-define("BASE_URL", "/free-code-camp/portfolio-php/"); // For links to front-end stuff
-define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/free-code-camp/portfolio-php/"); // For server-side stuff, like includes
+// define("BASE_URL", "/free-code-camp/portfolio-php/"); // For links to front-end stuff
+// define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/free-code-camp/portfolio-php/"); // For server-side stuff, like includes
 // Use for live site
-// define("BASE_URL", "/");
-// define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/");
+define("BASE_URL", "/");
+define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/");
 // define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/test/"); // For test site
