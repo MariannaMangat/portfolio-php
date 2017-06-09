55354b008e9c5e77abdb30c08245e0eeadd3da22
Tiffany Du <tadofdesign@gmail.com>
2017-06-09 15:02:05 -0500

    Created tribute project modal and updated teasers section

--- inc/portfolio-teasers.php
+++ inc/portfolio-teasers.php
@@ -1,23 +1,6 @@
 <div class="col-sm-6 col-lg-4">
   <div class="row">
     <div class="col-xs-12">
-      <a class="teaser" href="#" data-toggle="modal" data-target="#tributeModal">
-        <div>
-          <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/portfolio-teaser.jpg" alt="Personal Portfolio teaser">
-          <div class="overlay">
-            <div>
-              <h3>Personal Portfolio</h3>
-              <h4>Web Development | UI/UX Design | Logo &amp; Branding</h4>
-            </div>
-          </div>
-        </div>
-      </a>
-    </div>
-  </div>
-</div>
-<div class="col-sm-6 col-lg-4">
-  <div class="row">
-    <div class="col-xs-12">
       <a class="teaser" href="#" data-toggle="modal" data-target="#weatherModal">
         <div>
           <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/weather-teaser.jpg" alt="Weather app teaser">
--- modals/project-tribute.php
+++ modals/project-tribute.php
@@ -2,41 +2,53 @@
   <div id="tributeModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="tributeTitle">
     <div class="modal-dialog wide" role="document">
       <div class="modal-content text-left">
-        <div class="modal-header">
+        <div class="modal-header text-center">
           <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h3 id="tributeTitle" class="modal-title text-center">Project Details</h3>
+          <h2 id="tributeTitle" class="modal-title">Twenty One Pilots Tribute</h2>
+          <h4>Web Development</h4>
         </div><!-- /.modal-header -->
         <div class="modal-body">
-          <div class="row">
-            <div class="col-xs-12 bg-star-g">
-              <img class="img-responsive center-block hero" src="assets/portfolio/tribute/tribute-hero.png" alt="Tribute Page Hero Image">
+          <div class="flex-container">
+            <div class="flex-item full">
+              <img src="assets/portfolio/tribute/tribute-hero.jpg" alt="Tribute page hero">
             </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-12 text-center">
-              <h2>Twenty One Pilots Tribute</h2>
-              <h4>Responsive Web Design</h4>
+            <div class="flex-item full">
+              <div class="text">
+                <h4>Project Overview</h4>
+                <p>This tribute page was created for a FreeCodeCamp project. The goal was very simple: make a one-page site with images, text, and external links. I took this chance to practice using the Bootstrap 3 framework to create a tribute to one of my favorite bands at the moment, Twenty One Pilots (TOP). Lucky for me, they have such strong branding that the page nearly designed itself!</p>
+              </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-10 col-sm-offset-1 col-lg-6 col-lg-offset-0">
-              <p>This tribute page was created for a <a href="http://www.freecodecamp.com/" target="_blank" title="Link to FreeCodeCamp">FreeCodeCamp</a> project. The goal was very simple: make a one-page site with images, text, and external links. I took this chance to practice using the Bootstrap 3 framework to create a tribute to one of my favorite bands at the moment, Twenty One Pilots (TOP). Lucky for me, they have such strong branding that the page nearly designed itself!</p>
-              <p class="mb-30">I kept the color palette simple with black, white, and the signature TOP shade of red for the accent color. To add some visual interest, I used a subtle twill texture for the red backgrounds and styled the headings with a black background to give it a label maker tape effect. I also wrote some code that replaces the letter "O" with the scandinavian letter "&Oslash;" in the headings and navigation links to match the TOP logo.</p>
+            <div class="flex-item top">
+              <img src="assets/portfolio/tribute/tribute.gif" alt="Tribute page GIF">
             </div>
-            <div class="col-sm-10 col-sm-offset-1 col-lg-6 col-lg-offset-0 md-center">
-              <p><strong>Technologies Used:</strong></p>
-              <p class="mb-30">
-                <span class="label tag">Bootstrap 3</span>
-                <span class="label tag">Responsive Design</span>
-                <span class="label tag">HTML5</span>
-                <span class="label tag">CSS3</span>
-                <span class="label tag">JavaScript</span>
-                <span class="label tag">jQuery</span>
-              </p>
-              <a class="btn td-btn outline green mb-30" href="http://projects.tiffanydu.com/tribute/" target="_blank" title="Link to Tribute Page"><span class="glyphicon glyphicon-link"></span> Visit Live Site</a>
-              <a class="btn td-btn outline green mb-30 iconBtn" href="http://codepen.io/tiffanyadu/full/RKMavV/" target="_blank" title="Link to Tribute Page on CodePen"><img src="assets/icons/social/codepen-green.svg" alt="Codepen Icon">View on CodePen</a>
+            <div class="flex-item">
+              <img src="assets/portfolio/tribute/tribute-detail.jpg" alt="Tribute page detail">
             </div>
-          </div><!-- /.row -->
+            <div class="flex-item full">
+              <div class="text">
+                <h4>Design Decisions</h4>
+                <p>I kept the color palette simple with black, white, and the signature TOP shade of red for the accent color. To add some visual interest, I used a subtle twill texture for the red backgrounds and styled the headings with a black background to give it a label maker tape effect. I also wrote some code that replaces the letter "O" with the scandinavian letter "&Oslash;" in the headings and navigation links to match the TOP logo.</p>
+              </div>
+            </div>
+            <div class="flex-item full">
+              <img src="assets/portfolio/tribute/tribute-scene.jpg" alt="Tribute page scene mockup">
+            </div>
+            <div class="flex-item full mb-30">
+              <div class="text text-center">
+                <p><strong>Technologies Used:</strong></p>
+                <p class="mb-30">
+                  <span class="label tag">Bootstrap 3</span>
+                  <span class="label tag">Responsive Design</span>
+                  <span class="label tag">HTML5</span>
+                  <span class="label tag">CSS3</span>
+                  <span class="label tag">JavaScript</span>
+                  <span class="label tag">jQuery</span>
+                </p>
+                <a class="btn td-btn outline green mb-15" href="http://projects.tiffanydu.com/tribute/" target="_blank" title="Link to Tribute Page"><span class="glyphicon glyphicon-link"></span> Visit Live Site</a>
+                <a class="btn td-btn outline green iconBtn mb-15" href="https://github.com/tiffanyadu/tribute" target="_blank" title="Link to Tribute Page on GitHub"><img src="assets/icons/social/github-green.svg" alt="GitHub Icon">View on GitHub</a>
+              </div>
+            </div>
+          </div><!-- /.flex-container -->
           <div class="modal-footer center">
             <button class="btn td-btn outline small" type="button" data-dismiss="modal">Close Project</button>
           </div><!-- /.modal-footer -->
