bc11e6ecae186ef6e75ffa2dadde7616d6c9a8ca
Tiffany Du <tadofdesign@gmail.com>
2017-07-14 14:51:51 -0500

    Refreshed .gitignore

--- inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Use for local development
-// define("BASE_URL", "/free-code-camp/portfolio-php/"); // For links to front-end stuff
-// define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/free-code-camp/portfolio-php/"); // For server-side stuff, like includes
-// Use for live site
-define("BASE_URL", "/");
-define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/");
-// define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"] . "/test/"); // For test site
