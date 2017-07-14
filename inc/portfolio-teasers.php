99fbc5dd3b413552edf1e6772029011ec97dd09a
Tiffany Du <tadofdesign@gmail.com>
2017-07-14 14:50:25 -0500

    Added BechtelMGFX.com project

--- inc/portfolio-teasers.php
+++ inc/portfolio-teasers.php
@@ -1,6 +1,23 @@
 <div class="col-sm-6 col-lg-4">
   <div class="row">
     <div class="col-xs-12">
+      <a class="teaser" href="#" data-toggle="modal" data-target="#bechtelModal">
+        <div>
+          <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/placeholder-teaser.png" alt="BechtelMGFX teaser">
+          <div class="overlay">
+              <div>
+              <h3>BechtelMGFX.com</h3>
+              <h4>Web Design &amp; Development</h4>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+<div class="col-sm-6 col-lg-4">
+  <div class="row">
+    <div class="col-xs-12">
       <a class="teaser" href="#" data-toggle="modal" data-target="#weatherModal">
         <div>
           <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/weather-teaser.jpg" alt="Weather app teaser">
@@ -57,8 +74,9 @@
           <img class="img-responsive bg-shadow" src="assets/portfolio/teasers/placeholder-teaser.png" alt="Placeholder teaser">
           <div class="overlay">
               <div>
-              <h3>Ryan Bechtel - Motion Graphic Designer</h3>
-              <h4>Web Design &amp; Development</h4>
+              <h3>Your Company?</h3>
+              <h4>Let's talk about your website needs!</h4>
+              <button class="btn td-btn small outline pink mt-30" data-toggle="modal" data-target="#contactModal">Contact Me</button>
             </div>
           </div>
         </div>
